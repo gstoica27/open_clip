50e867a46da30929e248de680aeaae88a09cb3c8
George Stoica <georgestoica@Georges-MacBook-Pro.local>
2025-03-31 18:10:56 -0700

    bs 64 -> 512

--- run_exp.sh
+++ run_exp.sh
@@ -7,7 +7,7 @@ torchrun --nproc_per_node 8 \
     --val-data /weka/oe-training-default/mm-olmo/torch_datasets/pixmo_datasets/cap/validation \
     --train-num-samples 714985 \
     --dataset-type "hf" \
-    --batch-size 64 \
+    --batch-size 512 \
     --precision amp \
     --workers 8 \
     --epochs 30 \
