8aad92a78c1c051cc4ca862aeb2cfe19c8096ef7
root <root@triton-cs-aus-454.reviz.ai2.in>
2025-04-02 14:34:05 -0700

    Updated run script

--- run_exp.sh
+++ run_exp.sh
@@ -7,15 +7,16 @@ torchrun --nproc_per_node 8 \
     --val-data /weka/oe-training-default/mm-olmo/torch_datasets/pixmo_datasets/cap/validation \
     --train-num-samples 714985 \
     --dataset-type "hf" \
-    --batch-size 64 \
+    --batch-size 512 \
     --precision amp \
     --workers 8 \
     --epochs 30 \
     --imagenet-val "/weka/prior-default/georges/datasets/imagenet1k/imagenet/val" \
     --model "ViT-L-14-quickgelu" \
     --pretrained "metaclip_fullcc" \
-    --lr 1e-5 \
+    --lr 5e-6 \
     --wd 0.1 \
     --wandb-project-name metaclip-fullcc-pixmocap-ft \
     --report-to wandb \
+    --warmup 2e3
     # --logs /weka/oe_training_default/georges/checkpoints/finetuned_models/metaclip_400m/logs \
