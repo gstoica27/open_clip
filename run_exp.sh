6cfb32b05adb8d6f711d14a27994429acb4a5786
root <root@triton-cs-aus-454.reviz.ai2.in>
2025-04-02 15:14:51 -0700

    lower batch size + add accum

--- run_exp.sh
+++ run_exp.sh
@@ -7,7 +7,7 @@ torchrun --nproc_per_node 8 \
     --val-data /weka/oe-training-default/mm-olmo/torch_datasets/pixmo_datasets/cap/validation \
     --train-num-samples 714985 \
     --dataset-type "hf" \
-    --batch-size 512 \
+    --batch-size 256 \
     --precision amp \
     --workers 8 \
     --epochs 30 \
@@ -17,7 +17,7 @@ torchrun --nproc_per_node 8 \
     --lr 1e-5 \
     --wd 0.1 \
     --warmup 1000 \
-    --accum-freq 64 \
+    --accum-freq 128 \
     --report-to wandb \
     --wandb-project-name metaclip-fullcc-pixmocap-ft
     # --logs /weka/oe_training_default/georges/checkpoints/finetuned_models/metaclip_400m/logs \
