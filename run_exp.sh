94517ff00e18c350dfd86dd668301d675f6bc3a3
root <root@triton-cs-aus-454.reviz.ai2.in>
2025-04-02 18:48:50 -0700

    lowered batch size

--- run_exp.sh
+++ run_exp.sh
@@ -7,7 +7,7 @@ torchrun --nproc_per_node 8 \
     --val-data /weka/oe-training-default/mm-olmo/torch_datasets/pixmo_datasets/cap/validation \
     --train-num-samples 714985 \
     --dataset-type "hf" \
-    --batch-size 256 \
+    --batch-size 128 \
     --precision amp \
     --workers 8 \
     --epochs 30 \
@@ -17,7 +17,7 @@ torchrun --nproc_per_node 8 \
     --lr 1e-5 \
     --wd 0.1 \
     --warmup 1000 \
-    --accum-freq 128 \
+    --accum-freq 256 \
     --report-to wandb \
     --wandb-project-name metaclip-fullcc-pixmocap-ft
     # --logs /weka/oe_training_default/georges/checkpoints/finetuned_models/metaclip_400m/logs \
